7a95e5915188f9f808487b0db030b522924504a9
DikkeTimo <62908209+DikkeTimo@users.noreply.github.com>
2022-12-01 16:00:46 +0100

    Readme

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -45,3 +45,18 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+
+var btn = $("#button");
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass("show");
+  } else {
+    btn.removeClass("show");
+  }
+});
+
+btn.on("click", function (e) {
+  e.preventDefault();
+  $("main, html, body").animate({ scrollTop: 0 }, "300");
+});
